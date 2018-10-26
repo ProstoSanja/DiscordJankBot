e72bc472fb4a1c55cac6bb158e6d4e6198bff2a6
Alex Tsernoh <foralwork@gmail.com>
2018-10-26 13:10:05 +0100

    Morning  CS1P lecture

--- bot.js
+++ bot.js
@@ -29,27 +29,31 @@ client.on('message', message => {
   console.log(text)
 
   // If the message content starts with "!kick"
-  if (text.startsWith('!helpmewithalgebra')) {
+  if (text.startsWith ("!") && false) {
+    channel.send("NO")
+  } else if (text.startsWith('!helpmewithalgebra')) {
     text = text.substring(18)
     var pyProg = spawn('python', ["./relational_algebra.py", text]);
     pyProg.stdout.on('data', function (data) {
       data = data.toString('utf8');
-      channel.send("```css\n" + data + "\n```");
+      channel.send("```xml\n" + data + "\n```");
       reacttomessage(message, "DONE")
     });
   } else if (text.startsWith('!about') || text.startsWith('!help')) {
-    console.log(message.author.id)
+    message.delete()
     const embed = new RichEmbed()
       .setTitle("i'm bot made by Alex!")
       .setColor(0x00FF00)
       .setDescription("Here is what i can do:")
-      .addField("!about", "Show information anout me",true)
+      .addField("!about", "Show information anout me", true)
+      .addField("!poll", "Create quick YES/NO poll (beta)", true)
       .addField("!helpmewithalgebra", "Converts SQL query to relational algebra model (beta)", true)
       .addField("!react", "Adds your word as emoji letter reactions to previous message", true)
       .addField("!quote", "Inserts a random quote from a movie", true)
+      .addField("!suggest", "Any ideas?", true)
       .setAuthor("Alex Tsernoh", mymaster.avatarURL, "http://www.facebook.com/alex.tsernoh")
-      .setFooter("My source: https://github.com/ProstoSanja/DiscordJankBot   Feel free to contribute!", me.avatarURL)
-    channel.send(embed);
+      .setFooter("My source: https://github.com/ProstoSanja/DiscordJankBot   Feel free to contribute!", me.avatarURL);
+    message.author.send(embed);
   } else if (text.startsWith('!react ')) {
     text = text.substring(7).toUpperCase();
     message.delete()
@@ -64,6 +68,11 @@ client.on('message', message => {
       contents = contents.split("\n");
       message.channel.send('`' + contents[Math.floor(Math.random()*contents.length)] + '`');
     });
+  } else if (text.startsWith("!poll")) {
+    reacttomessage(message, "[]")
+  } else if (text.startsWith("!suggest") && message.author != me) {
+    message.delete();
+    mymaster.send(text + " ||| " + message.author.username)
   }
 });
 
